6ab04901b3a3c1de28c7c88fcd7ff69f392110c8
192mm <javier.18jun@gmail.com>
2022-09-23 19:14:51 -0500

    pokedex y pokemones

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const {pokemon} = require('./pokedex.json');
 
 /*
 Verbos HTTP
@@ -11,8 +12,41 @@ DELETE
 */
 app.get("/",(req,res,next) =>{
     res.status(200);
-    res.send("bienvenido");
+    res.send("Bienvenido al Pokedex");
 });
-app.listen(3000, () =>{
+/// para poner variables se usan ':'
+app.get('/pokemon/all',(req, res, next) =>{
+    res.status(200);
+    res.send(pokemon);
+})
+
+app.get('/pokemon/:id([0-9]{1,3})',(req, res, next) => {
+    const id = req.params.id - 1;
+    if( id >= 0 && id <= 150)
+    {
+        res.status(200);
+        res.send(pokemon[req.params.id - 1]);
+    }
+    else{
+        res.status(404);
+        res.send("Pokemon no encontrado");
+    }
+})
+
+app.get('/pokemon/:name',(req,res,next)=>{
+    const name = req.params.name;
+    for(i = 0; i < pokemon.length; i++ )
+    {
+        if (pokemon[i].name == name )
+        {
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.send("Pokemon no encontrado")
+})
+
+app.listen(process.env.PORT || 3000, () =>{
     console.log ('server is running');
 });
