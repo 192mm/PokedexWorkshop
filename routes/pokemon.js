ae03f79f99d9f3fbe7daf90dc64b91ee1cd4d3ef
192mm <javier.18jun@gmail.com>
2022-11-04 15:31:10 -0600

    post

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,13 +3,30 @@ const pokemon = express.Router();
 const db = require('../config/database');
 
 
-pokemon.post("/",(req,res,next) =>{
-    return res.status(200).json(req.body);
+pokemon.post("/", async (req, res, next) => {
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+
+    if (pok_name && pok_height && pok_weight && pok_base_experience) {
+        const query = "INSERT INTO pokemomn (pok_name, pok_height, pok_weight, pok_base_experience)";
+        //query += "VALUES("+req.body.pok_name+")";
+        query += `VALUES('${pok_name}',${pok_height}, ${pok_weight}, ${pok_base_experience})`
+
+        const rows = await db.query(query);
+
+        if (rows.affectedRows == 1) {
+            return res.status(201).json({ code: 201, message: "Pokemon insertado correctamente" });
+        }
+
+        return res.status(500).json({ code: 500, message: "Ocurrió un error" });
+        //return res.status(200).send(query);
+    }
+    return res.status(500).json({ code: 500, message: "Campos incompletos" });
+
 })
 
 pokemon.get('/', async (req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    res.status(200).json({code: 1, message: pkmn});
+    res.status(200).json({ code: 1, message: pkmn });
 })
 
 /// para poner variables se usan ':'
@@ -17,10 +34,10 @@ pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id;
     if (id > 0 && id <= 722) {
         const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = " + id)
-        res.status(200).json({code: 1, message: pkmn});
+        res.status(200).json({ code: 200, message: pkmn });
     }
     else {
-        res.status(404).res.send({code: 404, message: "Pokemoon no encontrado"});
+        res.status(404).res.send({ code: 404, message: "Pokemoon no encontrado" });
     }
 })
 // Expresiones regulares AKA REGEX: ([0-9]{1,3}) y ([A-Za-z]) etc...
@@ -48,12 +65,12 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
         return (p.pok_name.toUpperCase() == name.toUpperCase()) ?? p;
     })
 
-    /*console.log(pk);
-    if (pk.length > 0) {
-        return res.status(200).send(pk);
-    }
-    return res.status(404).send("Pokemon no encontrado")*/
-    (pk.length > 0) ? res.status(200).json({code: 1, message: pk}) : res.status(404).send({code:404, message:"Pokemon no encontrado"});
+        /*console.log(pk);
+        if (pk.length > 0) {
+            return res.status(200).send(pk);
+        }
+        return res.status(404).send("Pokemon no encontrado")*/
+        (pk.length > 0) ? res.status(200).json({ code: 200, message: pk }) : res.status(404).send({ code: 404, message: "Pokemon no encontrado" });
 })
 
 module.exports = pokemon; //la más sencilla de usar. Solo permite exportar una cosa
